41e171fcbaf382fc3acd5dc531df1c2c0183a9f0
ik12aki <bookproject-uf45.onrende.com>
2023-09-10 15:23:19 +0900

    Initial commit

---
+++ book/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Book , Review
+
+admin.site.register(Book)
+admin.site.register(Review)
+# from .models import SampleeMOdel
+# admin.site.register(SampleeMOdel)
+
